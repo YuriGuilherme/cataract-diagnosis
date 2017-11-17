0960148a14ab80b3a933e0b71ec7615dfb03ee8e
Unknown <erick_mmedeiros@hotmail.com>
2017-11-16 21:24:22 -0300

    main.c: Update open file process

--- src/main.c
+++ src/main.c
@@ -4,5 +4,20 @@
 #include "catdig.h"
 
 int main(void){
+
+	char x;
+	FILE *fotoin;
+	fotoin = fopen ("Catarata.ppm", "r");
+	if (fotoin == NULL) 
+	 {
+	 	printf("Erro na abertura do aquivo!\n");
+	 }
+	 else
+	{
+		fscanf(fotoin, &x);
+		printf("%d\n", x);
+		fclose(fotoin);
+	} 	
+	
   return 0;
 }
