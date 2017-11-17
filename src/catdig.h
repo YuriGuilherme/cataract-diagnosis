3235bbefb453dc4faf413fed25e41672b17db34e
yurigm97 <yurigm97@hotmail.com>
2017-11-16 22:33:44 -0300

    catdig.h: Update adding structs and enums for open files

--- src/catdig.h
+++ src/catdig.h
@@ -1,4 +1,18 @@
 #ifndef CATDIG_H
 #define CATDIG_H
 
+typedef struct {
+  int red;
+  int green;
+  int blue;
+} Pixel;
+
+typedef struct {
+  int height;
+  int width;
+  Pixel * pixels;
+} ppm;
+
+typedef enum {PPM} inputFormat;
+
 #endif /* CATDIG_H */
