b7ebb36cec9649d14596b6850e9b5e2e690c7675
yurigm97 <yurigm97@hotmail.com>
2017-12-04 16:47:29 -0300

    main: Update removing return images and mesages

--- src/main.c
+++ src/main.c
@@ -21,29 +21,5 @@ int main(int argc, char* argv[]) {
 
   __init__(argc, argv);
 
-  PPM *image = openFile(argv[2]);
-
-  if (image == NULL)
-    printf("Arquivo não é um PPM P3 ou não existe");
-
-  savePPMInFile("bin/normal.ppm", image);
-
-  image = colorToGrayscale(image);
-  savePPMInFile("bin/cinza.ppm", image);
-
-  image= ppmGaussianSmoothFilter(image);
-  savePPMInFile("bin/gaussian.ppm", image);
-
-  image = binarizacao(image);
-  savePPMInFile("bin/binaria.ppm", image);
-
-  image = ppmSobelSmoothFilter(image);
-  savePPMInFile("bin/sobel.ppm", image);
-
-  image = neighborhoodAnalysis(image);
-  savePPMInFile("bin/object.ppm", image);
-
-  cleanPPM(image);
-
   return 0;
 }
